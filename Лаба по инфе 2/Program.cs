d0fe6b6415816e78cee10df7d163c91d85f52959
Rango535 <rangorus2004@gmail.com>
2024-03-28 23:35:07 +0300

    first commit

---
+++ Лаба по инфе 2/Program.cs
@@ -0,0 +1,66 @@
+﻿class Game 
+{
+    public void factorial(int number) 
+    {
+        int result = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+
+        Console.WriteLine(result);
+    
+    }
+
+    public void fibonacci(int number) 
+    {
+        int prev = 0;
+        int noprev = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            int temp = prev;
+            prev = noprev;
+            noprev += temp;
+        }
+
+        Console.WriteLine(prev);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("""
+            Добро пожаловать в игру! Чтобы начать выберите один из вариантов:
+            Чтобы посчитать факториал введите 'факториал'
+            Чтобы узнать число из ряда Фибоначчи введите 'фибоначчи'
+            """);
+
+        Game frog = new Game();
+        try
+        {
+            switch (Console.ReadLine().ToLower().Trim())
+            {
+                case "факториал":
+                    Console.WriteLine("Введите искомое число");
+                    frog.factorial(int.Parse(Console.ReadLine()));
+                    break;
+
+                case "фибоначчи":
+                    Console.WriteLine("Введите искомое число");
+                    frog.fibonacci(int.Parse(Console.ReadLine()));
+                    break;
+
+                default:
+                    Console.WriteLine("Некорректное имя игры, пожалуйста, введите либо:\n'факториал', либо 'фибоначчи' (без кавычек)");
+                    break;
+            }
+        }
+
+        catch (Exception)
+        {
+            Console.WriteLine("Введен некорректный аргумент для вычисления!\nПожалуйста, вводите только числа");
+        }
+    }
+}
+
